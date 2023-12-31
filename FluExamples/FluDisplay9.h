4597b3ea122ab29dd26ec8c7af08698d36168526
mowangshuying <46436509+mowangshuying@users.noreply.github.com>
2024-01-01 05:22:11 +0800

    add display9.

---
+++ FluExamples/FluDisplay9.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <QWidget>
+#include <QGridLayout>
+
+// to display widget max can display 9 wdigets.
+class FluDisplay9 : public QWidget
+{
+    Q_OBJECT
+  public:
+    FluDisplay9(QWidget* parent = nullptr) : QWidget(parent)
+    {
+        gridLayout = new QGridLayout;
+        setLayout(gridLayout);
+        setStyleSheet("background-color:rbg(249, 249, 249");
+        resize(600, 400);
+    }
+
+
+    void addDemo(QWidget* widget)
+    {
+        if (gridLayout->count() >= 9)
+            return;
+
+        int count = gridLayout->count() + 1;
+        int row = count / 3;
+        int col = count % 3;
+
+        gridLayout->addWidget(widget, row, col);
+    }
+
+  protected:
+    QGridLayout* gridLayout;
+};
