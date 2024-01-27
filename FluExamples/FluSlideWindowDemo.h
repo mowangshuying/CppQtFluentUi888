b4348bfb822bca291844b27a463b65b21a98515b
mowangshuying <46436509+mowangshuying@users.noreply.github.com>
2024-01-27 12:45:03 +0800

    add FluSlideWindowDemo.

--- FluExamples/FluSlideWindowDemo.h
+++ FluExamples/FluSlideWindowDemo.h
@@ -5,6 +5,7 @@
 #include <QLabel>
 #include <QVBoxLayout>
 #include <QPropertyAnimation>
+#include <QHBoxLayout>
 
 class FluSlideWindowDemo : public QWidget
 {
@@ -12,10 +13,13 @@ class FluSlideWindowDemo : public QWidget
   public:
     FluSlideWindowDemo(QWidget* parent = nullptr) : QWidget(parent)
     {
-        m_sLayout = new QStackedLayout;
-        setLayout(m_sLayout);
+        //m_sLayout = new QStackedLayout;
+        setFixedSize(600, 400);
+        m_hLayout = new QHBoxLayout;
+        setLayout(m_hLayout);
 
-        m_page1 = new QWidget;
+        m_page1 = new QWidget(this);
+        m_page1->setFixedSize(600, 400);
         m_page1->setStyleSheet("background-color:red;");
         auto label1 = new QLabel;
         label1->setText("label1");
@@ -23,7 +27,10 @@ class FluSlideWindowDemo : public QWidget
         m_page1->setLayout(layout1);
         layout1->addWidget(label1);
 
-        m_page2 = new QWidget;
+        m_page2 = new QWidget(this);
+        m_page2->setFixedSize(600, 400);
+        m_page2->move(0, 600);
+
         m_page2->setStyleSheet("background-color:green;");
         auto label2 = new QLabel;
         label2->setText("label2");
@@ -32,7 +39,9 @@ class FluSlideWindowDemo : public QWidget
         layout2->addWidget(label2);
 
 
-        m_page3 = new QWidget;
+        m_page3 = new QWidget(this);
+        m_page3->move(0, 2 * 600);
+        m_page3->setFixedSize(600, 400);
         m_page3->setStyleSheet("background-color:blue;");
         auto label3 = new QLabel;
         label3->setText("label3");
@@ -40,31 +49,24 @@ class FluSlideWindowDemo : public QWidget
         m_page3->setLayout(layout3);
         layout3->addWidget(label3);
 
-        m_sLayout->addWidget(m_page1);
-        m_sLayout->addWidget(m_page2);
-        m_sLayout->addWidget(m_page3);
+       // m_hLayout->addWidget(m_page1);
+       // m_hLayout->addWidget(m_page2);
+       // m_hLayout->addWidget(m_page3);
     }
 
     void slideIt()
     {
-       // int curIndex = m_sLayout->currentIndex();
-        auto curWidget = m_sLayout->currentWidget();
-        auto nextWidget = m_sLayout->widget(m_sLayout->currentIndex() + 1);
-        if (curWidget == nullptr || nextWidget == nullptr)
-            return;
-
         // cur widget
-        QPropertyAnimation* curAnimation = new QPropertyAnimation(curWidget, "pos");
+        QPropertyAnimation* curAnimation = new QPropertyAnimation(m_page1, "pos");
         curAnimation->setDuration(500);
         curAnimation->setStartValue(QPoint(0, 0));
         curAnimation->setEndValue(QPoint(-width(), 0));
         curAnimation->start(QAbstractAnimation::DeleteWhenStopped);
 
-        m_sLayout->setCurrentWidget(nextWidget);
-
-
+        //m_hLayout->setCurrentWidget(nextWidget);
+        
         // next widget
-        QPropertyAnimation* nextAnimation = new QPropertyAnimation(nextWidget, "pos");
+        QPropertyAnimation* nextAnimation = new QPropertyAnimation(m_page2, "pos");
         nextAnimation->setDuration(500);
         nextAnimation->setStartValue(QPoint(width(), 0));
         nextAnimation->setEndValue(QPoint(0, 0));
@@ -86,5 +88,6 @@ class FluSlideWindowDemo : public QWidget
     QWidget* m_page1;
     QWidget* m_page2;
     QWidget* m_page3;
-    QStackedLayout* m_sLayout;
+    //QStackedLayout* m_sLayout;
+    QHBoxLayout* m_hLayout;
 };
