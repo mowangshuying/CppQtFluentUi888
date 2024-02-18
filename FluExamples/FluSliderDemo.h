a210ea1f9f3d1bfda9ddb93d078674496aecc9a1
mowangshuying <46436509+mowangshuying@users.noreply.github.com>
2024-02-19 04:47:47 +0800

    update FluSliderDemo.

--- FluExamples/FluSliderDemo.h
+++ FluExamples/FluSliderDemo.h
@@ -13,8 +13,16 @@ class FluSliderDemo : public QWidget
           hSlider->setMinimum(0);
           hSlider->setMaximum(100);
           hSlider->setSingleStep(10);
-
           hSlider->move(50, 50);
+
+          FluSlider* vSlider = new FluSlider(this);
+         // vSlider->setInvertedAppearance(false);
+          vSlider->setOrientation(Qt::Vertical);
+          vSlider->setMinimum(0);
+          vSlider->setMaximum(100);
+          vSlider->setSingleStep(5);
+          vSlider->move(250, 50);
+
           resize(600, 400);
       }
 };
