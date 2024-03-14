6648a7dff8c6c218746a0e70e1b0dc94d6bc4aac
mowangshuying <46436509+mowangshuying@users.noreply.github.com>
2024-03-15 07:18:17 +0800

    fix FluNumberBoxPage dark theme.

--- FluControls/FluDoubleSpinBox.h
+++ FluControls/FluDoubleSpinBox.h
@@ -10,5 +10,19 @@ class FluDoubleSpinBox : public QDoubleSpinBox
     FluDoubleSpinBox(QWidget* parent = nullptr) : QDoubleSpinBox(parent)
     {
         FluStyleSheetUitls::setQssByFileName("../StyleSheet/light/FluDoubleSpinBox.qss", this);
+        connect(FluThemeUtils::getUtils(), &FluThemeUtils::themeChanged, [=](FluTheme theme) { onThemeChanged(); });
+    }
+
+      public slots:
+    void onThemeChanged()
+    {
+        if (FluThemeUtils::getUtils()->getTheme() == FluTheme::Light)
+        {
+            FluStyleSheetUitls::setQssByFileName("../StyleSheet/light/FluDoubleSpinBox.qss", this);
+        }
+        else
+        {
+            FluStyleSheetUitls::setQssByFileName("../StyleSheet/dark/FluDoubleSpinBox.qss", this);
+        }
     }
 };
--- FluControls/FluSpinBox.h
+++ FluControls/FluSpinBox.h
@@ -10,5 +10,19 @@ class FluSpinBox : public QSpinBox
     FluSpinBox(QWidget* parent = nullptr) : QSpinBox(parent)
     {
         FluStyleSheetUitls::setQssByFileName("../StyleSheet/light/FluSpinBox.qss", this);
+        connect(FluThemeUtils::getUtils(), &FluThemeUtils::themeChanged, [=](FluTheme theme) { onThemeChanged(); });
+    }
+
+     public slots:
+    void onThemeChanged()
+    {
+        if (FluThemeUtils::getUtils()->getTheme() == FluTheme::Light)
+        {
+            FluStyleSheetUitls::setQssByFileName("../StyleSheet/light/FluSpinBox.qss", this);
+        }
+        else
+        {
+            FluStyleSheetUitls::setQssByFileName("../StyleSheet/dark/FluSpinBox.qss", this);
+        }
     }
 };
