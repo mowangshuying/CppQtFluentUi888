78b91d3e9ae3f8a075064fce963b3bac11dfc6ca
mowangshuying <46436509+mowangshuying@users.noreply.github.com>
2024-03-30 10:01:53 +0800

    update package.bat.

--- package.bat
+++ package.bat
@@ -1,13 +1,15 @@
+@REM定义字符串变量
+set curDir=%cd%
 @REM 删除所有文件
-rd /s /q E:\CodeLibraries\CppQtFluentUi888\FluGalleryWin64Msvc\
-mkdir E:\CodeLibraries\CppQtFluentUi888\FluGalleryWin64Msvc\package
+rd /s /q %curDir%\FluGalleryWin64Msvc\
+mkdir %curDir%\FluGalleryWin64Msvc\package
 @REM 拷贝所有文件
-cp -r E:\CodeLibraries\CppQtFluentUi888\res							E:\CodeLibraries\CppQtFluentUi888\FluGalleryWin64Msvc
-cp -r E:\CodeLibraries\CppQtFluentUi888\StyleSheet						E:\CodeLibraries\CppQtFluentUi888\FluGalleryWin64Msvc
-cp -r E:\CodeLibraries\CppQtFluentUi888\code						E:\CodeLibraries\CppQtFluentUi888\FluGalleryWin64Msvc
-cp E:\CodeLibraries\CppQtFluentUi888\x64\Release\FluWinGallery.exe 				E:\CodeLibraries\CppQtFluentUi888\FluGalleryWin64Msvc\package
-cp E:\CodeLibraries\CppQtFluentUi888\3rdparty\framelesshelper\bin\release\FramelessHelperCore64.dll 		E:\CodeLibraries\CppQtFluentUi888\FluGalleryWin64Msvc\package
-cp E:\CodeLibraries\CppQtFluentUi888\3rdparty\framelesshelper\bin\release\FramelessHelperQuick64.dll 		E:\CodeLibraries\CppQtFluentUi888\FluGalleryWin64Msvc\package
-cp E:\CodeLibraries\CppQtFluentUi888\3rdparty\framelesshelper\bin\release\FramelessHelperWidgets64.dll 	E:\CodeLibraries\CppQtFluentUi888\FluGalleryWin64Msvc\package
-C:\Qt\6.5.1\msvc2019_64\bin\windeployqt.exe E:\CodeLibraries\CppQtFluentUi888\FluGalleryWin64Msvc\package
+cp -r %curDir%\res							%curDir%\FluGalleryWin64Msvc
+cp -r %curDir%\StyleSheet					%curDir%\FluGalleryWin64Msvc
+cp -r %curDir%\code							%curDir%\FluGalleryWin64Msvc
+cp %curDir%\x64\Release\FluWinGallery.exe 											%curDir%\FluGalleryWin64Msvc\package
+cp %curDir%\3rdparty\framelesshelper\bin\release\FramelessHelperCore64.dll 			%curDir%\FluGalleryWin64Msvc\package
+cp %curDir%\3rdparty\framelesshelper\bin\release\FramelessHelperQuick64.dll 		%curDir%\FluGalleryWin64Msvc\package
+cp %curDir%\3rdparty\framelesshelper\bin\release\FramelessHelperWidgets64.dll 		%curDir%\FluGalleryWin64Msvc\package
+C:\Qt\6.5.1\msvc2019_64\bin\windeployqt.exe %curDir%\FluGalleryWin64Msvc\package
 pause
