c1800abf011dd2b6f6580b6a7fff0354e1e4a285
mowangshuying <46436509+mowangshuying@users.noreply.github.com>
2024-05-28 06:51:24 +0800

    optimize FluPivotTitleBarItem.

--- FluControls/FluPivotTitleBarItem.h
+++ FluControls/FluPivotTitleBarItem.h
@@ -3,6 +3,7 @@
 #include <QPushButton>
 #include "../FluUtils/FluUtils.h"
 #include <QStyle>
+#include <QVBoxLayout>
 
 class FluPivotTitleBarItem : public QPushButton
 {
@@ -10,25 +11,62 @@ class FluPivotTitleBarItem : public QPushButton
   public:
     FluPivotTitleBarItem(QWidget* parent = nullptr) : QPushButton(parent)
     {
+        m_vMainLayout = new QVBoxLayout;
+        setLayout(m_vMainLayout);
+        m_vMainLayout->setContentsMargins(5, 5, 5, 5);
+
+        m_textLabel = new QLabel;
+        m_textLabel->setWordWrap(true);
+        m_textLabel->setObjectName("textLabel");
+        m_vMainLayout->addWidget(m_textLabel);
+        m_vMainLayout->addWidget(m_textLabel, 1);
+
+        m_indicatorLabel = new QLabel;
+        m_indicatorLabel->setObjectName("indicatorLabel");
+        m_vMainLayout->addWidget(m_indicatorLabel);
+
+        m_indicatorLabel->setFixedHeight(5);
+
         setSelected(false);
+        setFixedHeight(45);
+
         FluStyleSheetUitls::setQssByFileName(":/StyleSheet/light/FluPivotTitleBarItem.qss", this);
+
+        connect(FluThemeUtils::getUtils(), &FluThemeUtils::themeChanged, this, [=]() { onThemeChanged();});
+        adjustItemSize();
     }
 
     void setSelected(bool bSelected)
     {
         setProperty("selected", bSelected);
+        m_textLabel->setProperty("selected", bSelected);
+        m_indicatorLabel->setProperty("selected", bSelected);
+
+        m_textLabel->style()->polish(m_textLabel);
+        m_indicatorLabel->style()->polish(m_indicatorLabel);
         style()->polish(this);
+
+        // adjustItemSize();
     }
 
     void setKey(QString key)
     {
         m_key = key;
-        setText(key);
+        m_textLabel->setText(key);
+        adjustItemSize();
     }
 
     QString getKey()
     {
         return m_key;
+    }
+
+    void adjustItemSize()
+    {
+        m_textLabel->show();
+        int nLabelWidth = m_textLabel->sizeHint().width();
+        m_textLabel->setFixedWidth(nLabelWidth);
+        setFixedWidth(nLabelWidth + 10);
     }
 
   public slots:
@@ -46,4 +84,7 @@ class FluPivotTitleBarItem : public QPushButton
 
   protected:
     QString m_key;
+    QLabel* m_textLabel;
+    QLabel* m_indicatorLabel;
+    QVBoxLayout* m_vMainLayout;
 };
