f9cec61c0eea5b383500a5d651f8ae02bf6cc47d
mowangshuying <46436509+mowangshuying@users.noreply.github.com>
2024-07-02 06:35:35 +0800

    update

---
+++ FluControls/FluTeachingTipContent.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "FluWidget.h"
+#include "FluTeachingTip.h"
+
+class FluTeachingTipContent : public FluWidget
+{
+    Q_OBJECT
+  public:
+      FluTeachingTipContent(QWidget* parent = nullptr) : FluWidget(parent)
+      {
+
+      }
+
+
+      void paintEvent(QPaintEvent* event)
+      {
+          FluWidget::paintEvent(event);
+      }
+  protected:
+      QColor m_borderC;
+      QColor m_brushC;
+
+      FluTeachingTipPos m_TipPos;
+
+      int m_nSpacing;
+};
