553bbbf9784b311649d489fe432b10454469b7ad
mowangshuying <46436509+mowangshuying@users.noreply.github.com>
2024-07-13 20:48:28 +0800

    adjust FluHyperLinkButton qss.

--- FluControls/FluHyperLinkButton.cpp
+++ FluControls/FluHyperLinkButton.cpp
@@ -3,6 +3,8 @@
 FluHyperLinkButton::FluHyperLinkButton(QString linkUrl, QWidget* parent /* = nullptr*/) : QPushButton(parent), m_linkUrl(linkUrl)
 {
     connect(this, &FluHyperLinkButton::clicked, [=](bool bClicked) { QDesktopServices::openUrl(QUrl(m_linkUrl)); });
-    FluStyleSheetUitls::setQssByFileName("../StyleSheet/light/FluHyperLinkButton.qss", this);
+    //FluStyleSheetUitls::setQssByFileName("../StyleSheet/light/FluHyperLinkButton.qss", this);
+    
+    onThemeChanged();
     connect(FluThemeUtils::getUtils(), &FluThemeUtils::themeChanged, this, [=](FluTheme theme) { onThemeChanged(); });
 }
