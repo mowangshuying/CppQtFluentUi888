0aa6704dfb2e5aba9dd7567ee6d55cb6ea7faafe
xutongxin <2656823393@qq.com>
2024-08-09 23:00:57 +0800

    Adds an overload constructor for FluPushButton to set button text

--- FluControls/FluPushButton.h
+++ FluControls/FluPushButton.h
@@ -6,33 +6,32 @@
 
 #include "../FluUtils/FluUtils.h"
 
-class FluPushButton : public QPushButton
-{
-    Q_OBJECT
-  public:
-    FluPushButton(QWidget *parent = nullptr) : QPushButton(parent)
-    {
-        setFixedSize(200, 30);
-        // setFixedHeight(30);
-        FluStyleSheetUitls::setQssByFileName("../StyleSheet/light/FluPushButton.qss", this);
-        if (FluThemeUtils::isDarkTheme())
-        {
-            FluStyleSheetUitls::setQssByFileName("../StyleSheet/dark/FluPushButton.qss", this);
-        }
-        connect(FluThemeUtils::getUtils(), &FluThemeUtils::themeChanged, this, [=](FluTheme theme) { onThemeChanged(); });
-    }
-  public slots:
-    void onThemeChanged()
-    {
-        if (FluThemeUtils::isLightTheme())
-        {
-            FluStyleSheetUitls::setQssByFileName("../StyleSheet/light/FluPushButton.qss", this);
-        }
-        else
-        {
-            FluStyleSheetUitls::setQssByFileName("../StyleSheet/dark/FluPushButton.qss", this);
-        }
-    }
+class FluPushButton : public QPushButton {
+        Q_OBJECT
 
-  protected:
+    public:
+        FluPushButton(QWidget *parent = nullptr) : QPushButton(parent) {
+            setFixedSize(200, 30);
+            // setFixedHeight(30);
+            FluStyleSheetUitls::setQssByFileName("../StyleSheet/light/FluPushButton.qss", this);
+            if (FluThemeUtils::isDarkTheme()) {
+                FluStyleSheetUitls::setQssByFileName("../StyleSheet/dark/FluPushButton.qss", this);
+            }
+            connect(FluThemeUtils::getUtils(), &FluThemeUtils::themeChanged, this,
+                    [=](FluTheme theme) { onThemeChanged(); });
+        }
+        explicit FluPushButton(const QString &text, QWidget *parent = nullptr) : FluPushButton(parent) {
+            setText(text);
+        }
+
+    public slots:
+        void onThemeChanged() {
+            if (FluThemeUtils::isLightTheme()) {
+                FluStyleSheetUitls::setQssByFileName("../StyleSheet/light/FluPushButton.qss", this);
+            } else {
+                FluStyleSheetUitls::setQssByFileName("../StyleSheet/dark/FluPushButton.qss", this);
+            }
+        }
+
+    protected:
 };
