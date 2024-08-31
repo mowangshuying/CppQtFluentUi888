47296d762c9698bc40eb72dfef08e9e15fafc2f6
mowangshuying <46436509+mowangshuying@users.noreply.github.com>
2024-09-01 05:27:45 +0800

    optimize code.

--- FluUtils/FluConfigUtils.cpp
+++ FluUtils/FluConfigUtils.cpp
@@ -1,1 +1,42 @@
 #include "FluConfigUtils.h"
+
+FluConfigUtils::FluConfigUtils(QObject* parent /*= nullptr*/) : QObject(parent)
+{
+    m_settings = new QSettings("../config/config.ini");
+}
+
+FluConfigUtils::~FluConfigUtils()
+{
+    delete m_settings;
+}
+
+FluTheme FluConfigUtils::getTheme()
+{
+    FluTheme theme;
+    m_settings->beginGroup("config");
+    QString themeStr = m_settings->value("theme", "Light").toString();
+    m_settings->endGroup();
+
+    if (themeStr == "Light")
+        theme = FluTheme::Light;
+    else
+        theme = FluTheme::Dark;
+    return theme;
+}
+
+void FluConfigUtils::setTheme(FluTheme theme)
+{
+    // config/theme
+    m_settings->beginGroup("config");
+
+    if (theme == FluTheme::Light)
+    {
+        m_settings->setValue("theme", "Light");
+    }
+    else
+    {
+        m_settings->setValue("theme", "Dark");
+    }
+
+    m_settings->endGroup();
+}
