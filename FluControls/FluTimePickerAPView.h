4152d6ee3f3ebbe46fdfea919c072b146bbc519d
mowangshuying <46436509+mowangshuying@users.noreply.github.com>
2024-10-01 07:09:32 +0800

    update

--- FluControls/FluTimePickerAPView.h
+++ FluControls/FluTimePickerAPView.h
@@ -21,242 +21,34 @@ class FluTimePickerAPView : public FluWidget
 {
     Q_OBJECT
   public:
-    FluTimePickerAPView(QWidget* parent = nullptr) : FluWidget(parent), m_bAm(true), m_hour(0), m_minute(0), m_bFirstShow(true), m_mask(nullptr)
-    {
-        setWindowFlags(Qt::Popup | Qt::FramelessWindowHint | Qt::NoDropShadowWindowHint);
-        setAttribute(Qt::WA_TranslucentBackground);
+    FluTimePickerAPView(QWidget* parent = nullptr);
 
-        m_hMainViewLayout = new QHBoxLayout;
-        setLayout(m_hMainViewLayout);
-        m_hMainViewLayout->setContentsMargins(8, 8, 8, 8);
+    int getHour();
 
-        m_mainView = new QFrame;
-        m_mainView->setObjectName("mainView");
-        m_hMainViewLayout->addWidget(m_mainView);
+    int getMinute();
 
-        m_vMainLayout = new QVBoxLayout;
-        m_vMainLayout->setContentsMargins(4, 4, 4, 4);
-        m_vMainLayout->setSpacing(0);
-        m_mainView->setLayout(m_vMainLayout);
+    void setHour(int hour);
 
-        m_hViewLayout = new QHBoxLayout;
-        // setLayout(m_hViewLayout);
-        m_vMainLayout->addLayout(m_hViewLayout);
+    void setMinute(int minute);
 
-        m_hourView = new FluLoopView(80);
-        m_minuteView = new FluLoopView(80);
-        m_ampmView = new FluAmPmView(80);
+    bool isAm();
 
-        m_mainView->setFixedWidth(248);
-        setFixedWidth(264);
+    void setAm(bool bAm);
 
-        // set hour data;
-        std::vector<QString> datas;
-        for (int i = 0; i < 12; i++)
-        {
-            QString str = QString::asprintf("%02d", i);
-            datas.push_back(str);
-        }
-        m_hourView->setAllItems(datas);
+    void updateTime();
 
-        // set minute data;
-        datas.clear();
-        for (int i = 0; i < 60; i++)
-        {
-            QString str = QString::asprintf("%02d", i);
-            datas.push_back(str);
-        }
-        m_minuteView->setAllItems(datas);
+    void setShadowEffect();
 
-        m_hViewLayout->setSpacing(0);
-        m_hViewLayout->addWidget(m_hourView);
-        m_hViewLayout->addWidget(new FluHSplitLine);
-        m_hViewLayout->addWidget(m_minuteView);
-        m_hViewLayout->addWidget(new FluHSplitLine);
-        m_hViewLayout->addWidget(m_ampmView);
+    void paintEvent(QPaintEvent* event);
 
-        m_hBtnLayout = new QHBoxLayout;
-        m_hBtnLayout->setContentsMargins(4, 4, 4, 4);
+    void showEvent(QShowEvent* event);
 
-        m_okBtn = new QPushButton;
-        m_cancelBtn = new QPushButton;
-
-        m_okBtn->setObjectName("okBtn");
-        m_cancelBtn->setObjectName("cancelBtn");
-
-        m_okBtn->setFixedHeight(40);
-        m_cancelBtn->setFixedHeight(40);
-
-        m_okBtn->setIconSize(QSize(25, 25));
-        m_okBtn->setIcon(FluIconUtils::getFluentIcon(FluAwesomeType::Accept));
-
-        m_cancelBtn->setIconSize(QSize(25, 25));
-        m_cancelBtn->setIcon(FluIconUtils::getFluentIconPixmap(FluAwesomeType::Cancel));
-
-        m_hBtnLayout->addWidget(m_okBtn);
-        m_hBtnLayout->addWidget(m_cancelBtn);
-
-        m_vMainLayout->addWidget(new FluVSplitLine);
-        m_vMainLayout->addLayout(m_hBtnLayout);
-
-        m_mask = new FluTimePickerViewMask(m_mainView);
-        m_mask->addItem("", 80, 40);
-        m_mask->addItem("", 80, 40);
-        m_mask->addItem("", 80, 40);
-
-        setMinute(0);
-        setHour(0);
-        setAm(true);
-
-        m_mask->setItemText(0, m_hourView->getCurrentText());
-        m_mask->setItemText(1, m_minuteView->getCurrentText());
-        m_mask->setItemText(2, m_ampmView->getCurrentText());
-
-        setShadowEffect();
-        connect(m_okBtn, &QPushButton::clicked, [=]() {
-            updateTime();
-            emit clickedOk();
-            close();
-        });
-        connect(m_cancelBtn, &QPushButton::clicked, [=]() {
-            emit clickedCancel();
-            close();
-        });
-
-        connect(m_hourView, &FluLoopView::visibaleMidIndexChanged, [=](int nIndex) { m_mask->setItemText(0, m_hourView->getCurrentText()); });
-
-        connect(m_minuteView, &FluLoopView::visibaleMidIndexChanged, [=](int nIndex) { m_mask->setItemText(1, m_minuteView->getCurrentText()); });
-
-        connect(m_ampmView, &FluAmPmView::currentItemChanged, [=]() { m_mask->setItemText(2, m_ampmView->getCurrentText()); });
-
-        connect(m_mask, &FluTimePickerViewMask::wheelChanged, [=](int nIndex, QWheelEvent* wheelEvent) {
-            if (nIndex == 0)
-                QApplication::sendEvent(m_hourView->viewport(), wheelEvent);
-            else if (nIndex == 1)
-                QApplication::sendEvent(m_minuteView->viewport(), wheelEvent);
-            else
-                QApplication::sendEvent(m_ampmView, wheelEvent);
-        });
-
-        connect(m_mask, &FluTimePickerViewMask::enterChanged, [=](int nIndex, QEnterEvent* event) {
-            // LOG_DEBUG << "nIndex:" << nIndex;
-            if (nIndex == 0)
-                QApplication::sendEvent(m_hourView, event);
-            else if (nIndex == 1)
-                QApplication::sendEvent(m_minuteView, event);
-            else
-                QApplication::sendEvent(m_ampmView, event);
-        });
-
-        connect(m_mask, &FluTimePickerViewMask::leaveChanged, [=](int nIndex, QEvent* event) {
-            // LOG_DEBUG << "nIndex:" << nIndex;
-            if (nIndex == 0)
-                QApplication::sendEvent(m_hourView, event);
-            else if (nIndex == 1)
-                QApplication::sendEvent(m_minuteView, event);
-            else
-                QApplication::sendEvent(m_ampmView, event);
-        });
-
-        FluStyleSheetUitls::setQssByFileName("../StyleSheet/light/FluTimePickerAPView.qss", this);
-    }
-
-    int getHour()
-    {
-        return m_hour;
-    }
-
-    int getMinute()
-    {
-        return m_minute;
-    }
-
-    void setHour(int hour)
-    {
-        m_hour = hour;
-        m_hourView->setVisibaleMidIndex(hour);
-        // m_hourView->scrollTo(hour);
-    }
-
-    void setMinute(int minute)
-    {
-        m_minute = minute;
-        m_minuteView->setVisibaleMidIndex(minute);
-        // m_hourView->scrollTo(minute);
-    }
-
-    bool isAm()
-    {
-        return m_bAm;
-    }
-
-    void setAm(bool bAm)
-    {
-        m_bAm = bAm;
-    }
-
-    void updateTime()
-    {
-        m_hour = m_hourView->getVisibleMidIndex();
-        m_minute = m_minuteView->getVisibleMidIndex();
-        m_bAm = m_ampmView->isAm();
-    }
-
-    void setShadowEffect()
-    {
-        // set shadow;
-        m_shadowEffect = new QGraphicsDropShadowEffect;
-        m_shadowEffect->setBlurRadius(8);
-        m_shadowEffect->setOffset(0, 0);
-        m_shadowEffect->setColor(QColor(0, 0, 0, 30));
-        if (FluThemeUtils::isDarkTheme())
-            m_shadowEffect->setColor(QColor(0, 0, 0, 80));
-        m_mainView->setGraphicsEffect(m_shadowEffect);
-    }
-
-    void paintEvent(QPaintEvent* event)
-    {
-        QStyleOption opt;
-        opt.initFrom(this);
-        QPainter painter(this);
-        style()->drawPrimitive(QStyle::PE_Widget, &opt, &painter, this);
-    }
-
-    void showEvent(QShowEvent* event)
-    {
-        FluWidget::showEvent(event);
-        if (m_bFirstShow)
-        {
-            m_bFirstShow = false;
-            m_hourView->scrollTo(m_hour);
-            m_minuteView->scrollTo(m_minute);
-        }
-    }
-
-    void resizeEvent(QResizeEvent* event)
-    {
-        m_mask->resize(m_mainView->width() - 8, 40);
-        m_mask->move(4, 166);
-    }
+    void resizeEvent(QResizeEvent* event);
   signals:
     void clickedOk();
     void clickedCancel();
   public slots:
-    void onThemeChanged()
-    {
-        if (FluThemeUtils::isLightTheme())
-        {
-            m_okBtn->setIcon(FluIconUtils::getFluentIcon(FluAwesomeType::Accept, FluTheme::Light));
-            m_cancelBtn->setIcon(FluIconUtils::getFluentIconPixmap(FluAwesomeType::Cancel, FluTheme::Light));
-            FluStyleSheetUitls::setQssByFileName("../StyleSheet/light/FluTimePickerAPView.qss", this);
-        }
-        else
-        {
-            m_okBtn->setIcon(FluIconUtils::getFluentIcon(FluAwesomeType::Accept, FluTheme::Dark));
-            m_cancelBtn->setIcon(FluIconUtils::getFluentIconPixmap(FluAwesomeType::Cancel, FluTheme::Dark));
-            FluStyleSheetUitls::setQssByFileName("../StyleSheet/dark/FluTimePickerAPView.qss", this);
-        }
-    }
+    void onThemeChanged();
 
   protected:
     QFrame* m_mainView;
